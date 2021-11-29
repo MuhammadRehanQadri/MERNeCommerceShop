9b4046c9662ea6bf6d28ef402ad1216179e54e0b
MuhammadRehanQadri <rehan.code@gmail.com>
2021-11-29 17:58:49 +0500

    Add to cart functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import Message from "../components/Message";
+import { addToCart } from "../actions/cartActions";
+import useQuery from "../utils/useQuery";
 
 const CartScreen = () => {
+  const params = useParams();
+
+  const productId = params.id;
+  const qty = Number(useQuery().get("qty"));
+  console.log(qty);
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  console.log(cartItems);
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return <h1>Hidsfadsfadsfasd</h1>;
 };
 
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -17,7 +17,7 @@ import Message from "../components/Message";
 import Rating from "../components/Rating";
 
 const ProductScreen = () => {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
 
   const dispatch = useDispatch();
   let params = useParams();
