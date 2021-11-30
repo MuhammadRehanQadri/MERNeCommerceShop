df6484f2b11ed648c6023eee666f0ed3ce213bb8
MuhammadRehanQadri <rehan.code@gmail.com>
2021-11-30 19:18:10 +0500

    Show user in Navbar & Logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,22 +1,44 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { logout } from "../actions/userActions";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
-        <Navbar.Brand href="/">Proshop</Navbar.Brand>
-        <Nav className="ml-auto">
-          <Nav.Link as={Link} to="/cart">
-            <i className="fa fa-shopping-cart"></i> Cart
-          </Nav.Link>
-          <Nav.Link as={Link} to="/login">
-            <i className="fa fa-user"> Sign In </i>
-          </Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">Proshop</Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/cart">
+              <i className="fa fa-shopping-cart"></i> Cart
+            </Nav.Link>
+            {userInfo ? (
+              <NavDropdown title={userInfo.name} id="username">
+                <NavDropdown.Item>Profile</NavDropdown.Item>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <Nav.Link as={Link} to="/login">
+                <i className="fa fa-user"> Sign In </i>
+              </Nav.Link>
+            )}
+          </Nav>
+        </Container>
+      </Navbar>
+    </header>
   );
 };
 
