9cf6212640199c0653c15f81ad18c1e582ec6167
MuhammadRehanQadri <rehan.code@gmail.com>
2021-11-30 14:53:58 +0500

    POST login

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,13 @@ import dotenv from "dotenv";
 
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 const app = express();
+
+app.use(express.json());
 
 connectDB();
 
@@ -15,6 +18,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
