47d9bfdee58a10d046b1c0df1a4d888c5c58e357
MuhammadRehanQadri <rehan.code@gmail.com>
2021-12-01 14:28:27 +0500

    PUT /api/users/profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/", registerUser);
 router.post("/login", authUser);
 router.get("/profile", protect, getUserProfile);
+router.put("/profile", protect, updateUserProfile);
 
 export default router;
